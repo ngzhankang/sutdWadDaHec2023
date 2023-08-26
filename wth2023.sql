091b6318cc3ebbb5ed76604cd6bea2943c489985
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-08-26 16:11:03 +0800

    Update wth2023.sql

--- wth2023.sql
+++ wth2023.sql
@@ -2,10 +2,11 @@ CREATE TABLE WTH2023
 (
     id INT PRIMARY KEY,
     name VARCHAR (200),
-    contact INT(8),
+    tutor_contact INT(8),
+    tutee_contact INT(8),
     dob DATETIME,
     gender VARCHAR(10),
-    tel INT(8),
+    house_tel INT(8),
     email VARCHAR (100),
     tutor_desc VARCHAR (255),
     strength_tutor VARCHAR (255),
@@ -18,7 +19,7 @@ CREATE TABLE WTH2023
     tutee_level VARCHAR(250),
     subjects VARCHAR(250),
     amt_discussion int(255),
-    session_hours DATETIME,
+    session_hours INT,
     session_date DATETIME
 );
 /* Use 10 for training
