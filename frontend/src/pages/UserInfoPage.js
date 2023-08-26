269d2db6da77debc4225954ea0c07aa7d51e8826
Nathanael <natsiewks@gmail.com>
2023-08-26 21:38:09 +0800

    update tutee info page

--- frontend/src/pages/UserInfoPage.js
+++ frontend/src/pages/UserInfoPage.js
@@ -27,6 +27,42 @@ export default function TutorUpdatePage() {
                 <p>hello</p>
             
             </StyledRoot>
+
+            <StyledRoot>
+                <h1>insert tutee name</h1>
+            </StyledRoot>
+
+            <StyledRoot>
+                <h1>tutor</h1>
+            </StyledRoot>
+
+            <StyledRoot>
+                <p>insert tutor name</p>
+            </StyledRoot>
+
+            <StyledRoot>
+                <h1>email</h1>
+            </StyledRoot>
+
+            <StyledRoot>
+                <p>insert email</p>
+            </StyledRoot>
+
+            <StyledRoot>
+                <h1>phone number</h1>
+            </StyledRoot>
+
+            <StyledRoot>
+                <p>insert phone number</p>
+            </StyledRoot>
+
+            <StyledRoot>
+                <h1>tutoring end date</h1>
+            </StyledRoot>
+
+            <StyledRoot>
+                <p>insert tutoring end date</p>
+            </StyledRoot>
         </>
     )
 }
