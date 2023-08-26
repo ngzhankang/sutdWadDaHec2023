f1202972b3e00d97d3e6b6b45df54483689736a7
unknown <limfelix16@gmail.com>
2023-08-26 22:52:04 +0800

    model predictions (but not accurate)

--- backend/app.py
+++ backend/app.py
@@ -13,9 +13,17 @@ def home():
 
 @app.route("/predict")
 def modelPredict():
-    d = model.get_params()['features']
+    input_text = [
+    "Today's tutoring session was truly rewarding. ... a resounding success.",
+    "I'm thrilled by my tutee's progress in essay structuring. ... promising step forward.",
+    "The chemistry review session was a pleasant surprise. ... truly motivating.",
+    "I had a terrible session today with my tutee. Their progress is inspiring.",
+    "Unfortunately, today's session didn't go well. My tutee seemed disengaged."
+    # ... (more training examples)
+]
+    d = model.predict(input_text)
     print(d)
-    return ""
+    return "asdf"
 
 if __name__ == "__main__":
     app.run(debug=True)
