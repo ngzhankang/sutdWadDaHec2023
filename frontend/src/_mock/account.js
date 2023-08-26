8225fd6221108008a5134b4ca23e4fd686c88c55
Nathanael <natsiewks@gmail.com>
2023-08-26 20:01:58 +0800

    change account name

--- frontend/src/_mock/account.js
+++ frontend/src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Jaydon Frankie',
+  displayName: 'Jonathan Tan',
   email: 'demo@minimals.cc',
   photoURL: '/assets/images/avatars/avatar_default.jpg',
 };
