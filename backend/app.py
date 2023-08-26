d7a3c292ea80c720f31b87e50eb1b9b939c71f4b
ngzhankang <zhankang.personal@gmail.com>
2023-08-26 19:26:17 +0000

    Revert to 073fda10fc9d651347b50ee8dd63b356f6cffaad

--- backend/app.py
+++ backend/app.py
@@ -15,10 +15,7 @@ def modelPredict():
     "I'm thrilled by my tutee's progress in essay structuring. ... promising step forward.",
     "The chemistry review session was a pleasant surprise. ... truly motivating.",
     "I had a terrible session today with my tutee. Their progress is inspiring.",
-    "Unfortunately, today's session didn't go well. My tutee seemed disengaged.",
-    "I had a great session with my tutee. They seemed interested and is willing to go the extra step to learn how differentiation actually works.",
-    "I had a fine session with my tutee. He seemed to be somewhat engaged and puts is some effort to really understand the steps needed to do the math problem that he was struggling in.",
-    "I'm ecstatic about my tutee's progress in Organic Chemistry. As after a week of guidance, she is finally able to understand alkenes, alkanes, and carboxylic acids."
+    "Unfortunately, today's session didn't go well. My tutee seemed disengaged."
     # ... (more training examples)
 ]
     d = model.predict(input_text)
