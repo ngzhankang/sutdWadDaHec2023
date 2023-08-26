2d70b8eff77fbc94101d2d6ffd2a6bca1921eaf3
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-08-27 01:41:06 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,9 @@ def modelPredict():
     "I'm thrilled by my tutee's progress in essay structuring. ... promising step forward.",
     "The chemistry review session was a pleasant surprise. ... truly motivating.",
     "I had a terrible session today with my tutee. Their progress is inspiring.",
-    "Unfortunately, today's session didn't go well. My tutee seemed disengaged."
+    "Unfortunately, today's session didn't go well. My tutee seemed disengaged.",
+    "I had a great session with my tutee. They seemed interested and is willing to go the extra step to learn how differentiation actually works.",
+    "I had a fine session with my tutee. He seemed to be somewhat engaged and puts is some effort to really understand the steps needed to do the math problem that he was struggling in. "
     # ... (more training examples)
 ]
     d = model.predict(input_text)
