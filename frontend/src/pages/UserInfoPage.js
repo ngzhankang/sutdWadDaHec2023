215d782c9b3bbf05fd5ab43d20797a7242cad004
Nathanael <natsiewks@gmail.com>
2023-08-26 23:34:06 +0800

    add accordion

--- frontend/src/pages/UserInfoPage.js
+++ frontend/src/pages/UserInfoPage.js
@@ -1,7 +1,8 @@
 import { Helmet } from 'react-helmet-async';
 // @mui
 import { styled } from '@mui/material/styles';
-import { Link, Container, Typography, TextField, Divider, Stack, Button } from '@mui/material';
+import { Link, Container, Typography, TextField, Divider, Stack, Button, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+
 // hooks
 import useResponsive from '../hooks/useResponsive';
 // components
@@ -19,12 +20,38 @@ export default function TutorUpdatePage() {
     const mdUp = useResponsive('up', 'md');
     return (
         <>
+                <Accordion>
+                <AccordionSummary
+                >
+                <Typography>Accordion 1</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+                </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                <AccordionSummary
+                >
+                <Typography>Accordion 2</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+                </AccordionDetails>
+            </Accordion>
+
             <Helmet>
                 <title> Update tutor </title>
             </Helmet>
 
             <StyledRoot>
-                <p>hello</p>
+                
+                <p>hi</p>
             
             </StyledRoot>
         </>
