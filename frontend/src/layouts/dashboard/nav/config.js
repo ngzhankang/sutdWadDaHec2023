3d37e5973511deabd0e8bc5927976e2473df44cf
ngzhankang <zhankang.personal@gmail.com>
2023-08-26 09:38:04 +0000

    some stuph

--- frontend/src/layouts/dashboard/nav/config.js
+++ frontend/src/layouts/dashboard/nav/config.js
@@ -22,11 +22,6 @@ const navConfig = [
     icon: icon('ic_cart'),
   },
   {
-    title: 'blog',
-    path: '/dashboard/blog',
-    icon: icon('ic_blog'),
-  },
-  {
     title: 'login',
     path: '/login',
     icon: icon('ic_lock'),
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,7 +3,6 @@ import { Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from './layouts/dashboard';
 import SimpleLayout from './layouts/simple';
 //
-import BlogPage from './pages/BlogPage';
 import UserPage from './pages/UserPage';
 import LoginPage from './pages/LoginPage';
 import Page404 from './pages/Page404';
@@ -21,8 +20,7 @@ export default function Router() {
         { element: <Navigate to="/dashboard/app" />, index: true },
         { path: 'app', element: <DashboardAppPage /> },
         { path: 'user', element: <UserPage /> },
-        { path: 'products', element: <ProductsPage /> },
-        { path: 'blog', element: <BlogPage /> },
+        { path: 'products', element: <ProductsPage /> }
       ],
     },
     {
