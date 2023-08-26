909120f53fb84260f54f6e2ddbae6451300bb37a
ngzhankang <zhankang.personal@gmail.com>
2023-08-26 10:32:17 +0000

    changesss

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,8 +3,6 @@ import { Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from './layouts/dashboard';
 import SimpleLayout from './layouts/simple';
 //
-import UserPage from './pages/UserPage';
-import LoginPage from './pages/LoginPage';
 import Page404 from './pages/Page404';
 import DashboardAppPage from './pages/DashboardAppPage';
 
