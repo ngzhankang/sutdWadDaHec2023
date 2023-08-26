e411144ee1ff0a3edb71ed90c178fbe837eb64b0
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-08-26 13:47:44 +0800

    Create wth2023.sql

---
+++ wth2023.sql
@@ -0,0 +1,14 @@
+CREATE TABLE WTH2023
+{
+    id int primary key,
+    name varchar(100),
+    contact int,
+    dob datetime,
+    gender varchar(15),
+    tel int,
+    email varchar(150),
+    tutor_desc varchar(300)
+};
+/* Use 10 for training
+300 for total Datasets 
+*/
