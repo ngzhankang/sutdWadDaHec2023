d9497b75f50f3c018a13b9958128a39c9001fb90
Nathanael <natsiewks@gmail.com>
2023-08-27 09:36:07 +0800

    compiled into single json

--- frontend/src/_mock/user.js
+++ frontend/src/_mock/user.js
@@ -1,8 +1,13 @@
 import { faker } from '@faker-js/faker';
 import { sample } from 'lodash';
+import tutees from '../data/tutee'
+import tutors from '../data/tutor'
 
 // ----------------------------------------------------------------------
 
+
+
+/*
 const users = [...Array(24)].map((_, index) => ({
   id: faker.datatype.uuid(),
   avatarUrl: `/assets/images/avatars/avatar_${index + 1}.jpg`,
@@ -12,5 +17,7 @@ const users = [...Array(24)].map((_, index) => ({
   status: Math.floor(Math.random() * 10).toString(),
   role: sample(['P', 'S']).concat("", Math.floor(Math.random() * 4 + 1).toString(), " ", sample(['Math', 'Eng', 'Sci']))
 }));
+
+*/
 
 export default users;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,29 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<App />);
 
+/*
+
+function createDbConnection() {
+    const db = new sqlite3.Database(filepath, (error) => {
+        if (error) {
+            return console.error(error.message)
+        }
+    })
+    createTutor(db)
+    var userInsert = 'INSERT INTO createTutor (name, email. phone, endDate) VALUES (?, ?, ?, ?)'
+    db.run(userInsert, ['i am john', '123@gmail.com', '999', '1/1/2023'])
+}
+
+function createTutor(db) {
+    db.run(`CREATE TABLE User (
+        name TEXT,
+        email TEXT,
+        phone INT,
+        endDate TEXT)`)
+}
+
+*/
+
 // If you want to enable client cache, register instead.
 serviceWorker.unregister();
 
